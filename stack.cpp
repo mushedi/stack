2c57f9f57bf524c55a66d99e229cad7bc83a6ce7
Azul <svcredblu@gmail.com>
2022-11-23 12:47:08 -0600

    implemented stack constructor

--- stack.cpp
+++ stack.cpp
@@ -2,3 +2,22 @@
 
 #include "stack.h"
 
+Stack::Stack(int size){
+
+    top = -1;
+
+    std::smatch m;
+    std::regex e("[^0-9$]");
+    string s = to_string(size);
+
+    if(size < 2 || !regex_search(s,m,e)){
+
+        size = DEFAULT;
+
+    }
+
+    stack = new Data*[size];
+
+    
+    
+}
